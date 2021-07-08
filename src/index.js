080f8d8015e80a8d288d6e38a499e9e767acc95c
Luiz Eduardo Satélis dos Santos <edusatelis@gmail.com>
2021-07-07 23:31:47 -0300

    Accept payload large

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors())
-app.use(express.json());
+app.use(express.json({limit: 52428800}));
 app.use(express.urlencoded({extended: true}));
 Routes.init(app);
 
